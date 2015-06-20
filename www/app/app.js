eef9ba5d8194c7282ca0ee89f251be79a93662b1
Akki281 <akkib2888@gmail.com>
2015-06-20 13:34:45 +0100

    Initial Commit with github as well

---
+++ www/app/app.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Aakanksha on 14/06/2015.
+ */
+angular.module("starterApp", ["ionic"])
+
+
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+
+            .state('home', {
+
+                url : "/home",
+                templateUrl : "app/home/home.html"
+
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/home');
+    });
